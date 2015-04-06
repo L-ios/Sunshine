a91ad7a44134709e9f39210c7ff4af476f251cc7
Lionseun Ling <lionseun@gmail.com>
2015-04-06 16:58:30 +0800

    First Commit

---
+++ app/src/main/java/ower/lios/android/sunshine/app/MainActivity.java
@@ -0,0 +1,16 @@
+package ower.lios.android.sunshine.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lionseun on 4/6/15.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+    }
+
+}
